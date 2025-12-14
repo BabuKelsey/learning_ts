fb87d3d4a4e56e1313e73244c126860c15c5e0b2
Kelsey Katie Sara Anastasia <PcVOXels@gmail.com>
2025-12-14 15:37:50 +0800

    understanding comparisons still

---
+++ out/comparisonTypes.js
@@ -0,0 +1,9 @@
+function doubleEquals(a, b) {
+    return a == b;
+}
+function tripleEquals(a, b) {
+    return a === b;
+}
+console.log(doubleEquals(5, "5")); // true
+console.log(tripleEquals(5, "5")); // false
+//# sourceMappingURL=comparisonTypes.js.map
---
+++ source/unknownTypes.ts
@@ -0,0 +1,13 @@
+function compareValues(a: unknown): string {    
+    if (typeof a === "number") {
+        return `"${a}" is a number.`;
+    } else if (typeof a === "string") {
+        return `"${a}" is a string.`;
+    }
+    
+    return `"${a}" data type is unchecked.`;
+}
+
+console.log(compareValues(42));             // Output: "42 is a number."
+console.log(compareValues("hello"));        // Output: "hello is a string."
+console.log(compareValues(true));           // Output: "true data type is unchecked."
