264125830f254135cf06c5777a03d5f38f09fde9
Kelsey Katie Sara Anastasia <PcVOXels@gmail.com>
2025-12-15 07:37:49 +0800

    1.0.0
    versions now a thing :P
    wrf theres emojis :trollface:

--- source/unknownTypes.ts
+++ source/unknownTypes.ts
@@ -8,6 +8,22 @@ function compareValues(a: unknown): string {
     return `"${a}" data type is unchecked.`;
 }
 
+function compareValuesNullCatch(a: any): any {
+    if (typeof a === "number" && a != null) {
+        return `"${a}" is a number.`;
+    } else if (typeof a === "string" && a != null) {
+        return `"${a}" is a string.`;
+    }
+    return `"${a}" data type is unchecked.`;
+}
+
 console.log(compareValues(42));             // Output: "42 is a number."
 console.log(compareValues("hello"));        // Output: "hello is a string."
 console.log(compareValues(true));           // Output: "true data type is unchecked."
+
+console.log(compareValuesNullCatch(42));             // Output: "42 is a number."
+console.log(compareValuesNullCatch("hello"));        // Output: "hello is a string."
+console.log(compareValuesNullCatch(true));           // Output: "true data type is unchecked."
+console.log(compareValuesNullCatch(null));           // Output: "true data type is unchecked."
+
+// the ai auto fill is annoying
