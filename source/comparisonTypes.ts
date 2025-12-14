865eab8b50f1d7a5f6197d7161ca9598a26cf002
Kelsey Katie Sara Anastasia <PcVOXels@gmail.com>
2025-12-15 07:39:37 +0800

    1.1.0
    moved comparison function to the appropriate file.
    
    files might still not be fully accurate, but atp, idrc

--- source/comparisonTypes.ts
+++ source/comparisonTypes.ts
@@ -6,5 +6,19 @@ function tripleEquals(a: any, b: any): boolean {
     return a === b;
 }
 
+function compareValuesNullCatch(a: any): any {
+    if (typeof a === "number" && a != null) {
+        return `"${a}" is a number.`;
+    } else if (typeof a === "string" && a != null) {
+        return `"${a}" is a string.`;
+    }
+    return `"${a}" data type is unchecked.`;
+}
+
 console.log(doubleEquals(5, "5"));  // true
 console.log(tripleEquals(5, "5"));  // false
+
+console.log(compareValuesNullCatch(42));             // Output: "42 is a number."
+console.log(compareValuesNullCatch("hello"));        // Output: "hello is a string."
+console.log(compareValuesNullCatch(true));           // Output: "true data type is unchecked."
+console.log(compareValuesNullCatch(null));           // Output: "true data type is unchecked."
