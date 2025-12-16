bf2a1cfce8b1f2de9c97d8792e12352eb0006d73
Kelsey Katie Sara Anastasia <PcVOXels@gmail.com>
2025-12-16 10:34:40 +0800

    1.2.2
    console.log updates

--- source/array.ts
+++ source/array.ts
@@ -12,11 +12,11 @@ console.log(printArray(saidArray));
 //console.log(saidArray[0]);
 
 // array modification methods
-saidArray.push(4); // adds an element at the end
-console.log(".push(): " + printArray(saidArray));
+saidArray.push(4);
+console.log(".push() adds an element at the end: " + printArray(saidArray));
 
-saidArray.pop(); // removes the last element
-console.log(".pop(): " + printArray(saidArray));
+saidArray.pop();
+console.log(".pop() removes the last element: " + printArray(saidArray));
 
 saidArray.splice(5, 0, 1);
 console.log(".splice() and add an element at index 5: " + printArray(saidArray));
@@ -24,17 +24,20 @@ console.log(".splice() and add an element at index 5: " + printArray(saidArray))
 saidArray.splice(3, 1);
 console.log(".splice() and removes the element at index 3: " + printArray(saidArray));
 
-saidArray.unshift(0); // adds an element at the start
-console.log(".unshift(): " + printArray(saidArray));
+saidArray.unshift(0);
+console.log(".unshift() adds an element at the start: " + printArray(saidArray));
 
-saidArray.shift(); // removes the first element
-console.log(".shift(): " + printArray(saidArray));
+saidArray.shift();
+console.log(".shift() removes the first element: " + printArray(saidArray));
 
-saidArray = saidArray.concat([4, 5, 6, 7, 8]); // concatenates another array
-console.log(".concat(): " + printArray(saidArray));
+saidArray = saidArray.concat([4, 5, 6, 7, 8]);
+console.log(".concat() concatenates another array: " + printArray(saidArray));
 
-saidArray.sort((a, b) => b - a); // sorts in descending order
-console.log(".sort(): " + printArray(saidArray));
+saidArray.sort();
+console.log(".sort() sorts in ascending order in this case: " + printArray(saidArray));
 
-saidArray.reverse(); // reverses the array
-console.log(".reverse(): " + printArray(saidArray));
+saidArray.sort((a, b) => b - a);
+console.log(".sort() sorts in descending order in this case: " + printArray(saidArray));
+
+saidArray.reverse();
+console.log(".reverse() reverses the array: " + printArray(saidArray));
